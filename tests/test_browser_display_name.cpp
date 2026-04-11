dc087039f4206ee3a3368ea63abdc78c60bde1ea
Rodrigo Iglesias <8595185+RigleGit@users.noreply.github.com>
2026-04-11 11:50:15 +0200

    test(browser): add null guards and two new edge-case tests

--- tests/test_browser_display_name.cpp
+++ tests/test_browser_display_name.cpp
@@ -8,12 +8,18 @@ static void TestEmptyTitle() {
 
 static void TestNullTitle() {
   const char *r = BrowserDisplayNameSource(nullptr, "foo.epub");
+  test::ExpectNotNull("null title result", r);
   test::ExpectStrEq("null title", r, "foo.epub");
 }
 
 static void TestTitleEqualsStem() {
   const char *r = BrowserDisplayNameSource("foo", "foo.epub");
   test::ExpectStrEq("title equals stem", r, "foo.epub");
+}
+
+static void TestTitleEqualsStemCaseInsensitive() {
+  const char *r = BrowserDisplayNameSource("Foo", "foo.epub");
+  test::ExpectStrEq("title equals stem case-insensitive", r, "foo.epub");
 }
 
 static void TestTitleDifferentFromStem() {
@@ -28,16 +34,24 @@ static void TestTitleWithSpecialChars() {
 
 static void TestNullFilename() {
   const char *r = BrowserDisplayNameSource("My Book", nullptr);
+  test::ExpectNotNull("null filename result", r);
   test::ExpectStrEq("null filename", r, "My Book");
+}
+
+static void TestEmptyFilename() {
+  const char *r = BrowserDisplayNameSource("My Book", "");
+  test::ExpectStrEq("empty filename", r, "My Book");
 }
 
 int main() {
   TestEmptyTitle();
   TestNullTitle();
   TestTitleEqualsStem();
+  TestTitleEqualsStemCaseInsensitive();
   TestTitleDifferentFromStem();
   TestTitleWithSpecialChars();
   TestNullFilename();
+  TestEmptyFilename();
   printf("All BrowserDisplayNameSource tests passed.\n");
   return 0;
 }
